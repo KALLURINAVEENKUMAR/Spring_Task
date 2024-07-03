816b9bed4167f49df26c38251d32814e374832c7
KALLURINAVEENKUMAR <naveen.kalluri@practo.com>
2024-07-03 12:30:58 +0530

    first commit

---
+++ src/main/java/com/naveen/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.naveen.controllers;
+
+import com.naveen.Model.UserModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    private Map<String, UserModel> userMap = new HashMap<>();
+
+    public UserController() {
+        userMap.put("John", new UserModel("John", "Doe", 1234));
+        userMap.put("Jane", new UserModel("Jane", "Doe", 5432));
+    }
+
+    @GetMapping("/getUser/{userName}")
+    public UserModel getUser(@PathVariable String userName) {
+        return userMap.get(userName);
+    }
+}
